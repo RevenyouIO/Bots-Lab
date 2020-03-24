fe358dec41c45f874dcd018b98ab3818f5e473dd
Raoul van de Oudeweetering <raoul.van.de.oudeweetering@carthago-ict.nl>
2020-03-24 17:41:09 +0100

    add signalId

--- run_live.py
+++ run_live.py
@@ -1,4 +1,5 @@
 import requests
+import uuid
 import importlib
 
 from config_live import datasource, buy_signal_settings, revenyou_api_url, bot_name
@@ -14,17 +15,18 @@ def run_bot(data):
     bot = import_bot(name = bot_name)
     buy_or_sell_signal = bot.get_buy_or_sell_signal(data)
     if buy_or_sell_signal == 'buy':
-        revenyou_api_signal = create_revenyou_api_signal()
+        revenyou_api_signal = create_revenyou_buy_signal()
         request = requests.post(url = revenyou_api_url.strip("\n"), data = revenyou_api_signal, headers = {'content-type': 'application/json'})
         print(request.json())
 
-def create_revenyou_api_signal():
+def create_revenyou_buy_signal():
     api_signal = {    
         "signalProvider": buy_signal_settings.get('signal_provider'),
         "signalProviderKey": buy_signal_settings.get('signal_provider_key'),
         "exchange": buy_signal_settings.get('exchange'),
         "symbol": buy_signal_settings.get('symbol'),
         "signalType": 'buy',
+        "signalId": str(uuid.uuid4())
     }
 
     return api_signal
