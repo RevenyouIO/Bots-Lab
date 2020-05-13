83abf86ef2fabb84f2eb39aa74e0020e2440305f
Raoul van de Oudeweetering <raoul.van.de.oudeweetering@carthago-ict.nl>
2020-05-13 18:00:43 +0200

    WIP again

--- websocket_binance.py
+++ websocket_binance.py
@@ -8,42 +8,47 @@ try:
     import thread
 except ImportError:
     import _thread as thread
-import time
 
 from data.data_service import data_settings_binance
 
-class BinanceWebsocketClient():
+class BinanceWebsocketClient:
     
     def __init__(self):
         self.ticker_data_array = []
         self.previous_tick = 0
 
-    def on_message(ws, message):
+    def on_message(self, ws, message):
         ticker_data = json.loads(message)
-        if int(ticker_data['E']) - self.previous_tick < 5000:
-            self.previous_tick = int(ticker_data['E'])
-            print('too soon')
+
+        # skip this tick when it is too soon
+        if (int(ticker_data['E']) - self.previous_tick) < 5000:
             return
 
         self.previous_tick = int(ticker_data['E'])
+
         print(message)
 
         self.ticker_data_array.append(ticker_data)
    
-      if len(self.ticker_data_array) > 5:
+        # limit the ticker data array size
+        if len(self.ticker_data_array) > 20:
             self.ticker_data_array.pop(0)
         
         df = pd.DataFrame(self.ticker_data_array)
+        df['E'] = pd.to_datetime(df['E'], unit='ms')
+        df.columns = ['type', 'date', 'symbol', 'close', 'open', 'high', 'low', 'volume', 'volume_quote']
+
+        # roep bot aan, en stuur eventueel buy signal
         print('data frame: ')
         print(df)
 
-    def on_error(ws, error):
+    def on_error(self, ws, error):
         print("Websocker error: {}", error)
 
-    def on_close(ws):
+    def on_close(self, ws):
         print("Websocket closed")
 
-    def on_open(ws):
+    def on_open(self, ws):
         ws.send({
             "method": "SUBSCRIBE",
             "params": [
@@ -52,12 +57,13 @@ class BinanceWebsocketClient():
             "id": 1
         })
 
-    def listen():
+    def listen(self):
         websocket.enableTrace(True)
         uri = 'wss://stream.binance.com:9443/ws/{}@miniTicker'.format(data_settings_binance.get('pair').lower())
         ws = websocket.WebSocketApp(uri,
-                                  on_message = on_message,
-                                  on_error = on_error,
-                                  on_close = on_close)
-        ws.on_open = on_open
+                                on_message = lambda ws,msg: self.on_message(ws, msg),
+                                on_error   = lambda ws,msg: self.on_error(ws, msg),
+                                on_close   = lambda ws: self.on_close(ws),
+                                on_open    = lambda ws: self.on_open(ws))
+        ws.on_open = lambda ws: self.on_open(ws)
         ws.run_forever()
