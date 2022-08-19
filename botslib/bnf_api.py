b484789a05220d133f7b73e26e3212bb049a5e12
MarjanCinnamon <marjan.stojanov@cinnamon.agency>
2022-08-19 07:56:10 +0200

    Add BNF API Wrapper

--- botslib/bnf_api.py
+++ botslib/bnf_api.py
@@ -11,11 +11,22 @@ import json
 import os
 
 import requests
+from botslib.bnf_schemas import (
+    BaseQueryStringSchema,
+    BorrowRepaySchema,
+    CancelOrderSchema,
+    CreateOrderSchema,
+    GetOrderQueryStringSchema,
+    GetOrdersQueryStringSchema,
+)
 
 
 class BNF_API:
     """
     BNF API Wrapper
+
+    Raises:
+        marshmallow.exceptions.ValidationError
     """
 
     def __init__(self):
@@ -25,33 +36,42 @@ class BNF_API:
         self.secret = os.environ.get("BOT_SECRET")
 
     def available_symbols(self):
+        params = BaseQueryStringSchema().load(params)
         return self._make_request(endpoint="/symbols")
 
     def balance(self):
+        params = BaseQueryStringSchema().load(params)
         return self._make_request(endpoint="/balance")
 
     def balance_in_bot_base_asset(self):
+        params = BaseQueryStringSchema().load(params)
         return self._make_request(endpoint="/balance/total")
 
-    def list_open_orders(self, params):
+    def list_open_orders(self, params: dict):
+        params = GetOrdersQueryStringSchema().load(params)
         return self._make_request(endpoint="/orders/list-open", params=params)
 
-    def get_order_by_id(self, params):
+    def get_order_by_id(self, params: dict):
+        params = GetOrderQueryStringSchema().load(params)
         return self._make_request(endpoint="/orders/status", params=params)
 
-    def create_order(self, data):
+    def create_order(self, data: dict):
+        data = CreateOrderSchema().load(data)
         return self._make_request(endpoint="/orders/place", data=data)
 
-    def cancel_order(self, data):
+    def cancel_order(self, data: dict):
+        data = CancelOrderSchema().load(data)
         return self._make_request(endpoint="/orders/cancel", data=data)
 
-    def borrow(self, data):
+    def borrow(self, data: dict):
+        data = BorrowRepaySchema().load(data)
         return self._make_request(endpoint="/loans/borrow", data=data)
 
-    def repay(self, data):
+    def repay(self, data: dict):
+        data = BorrowRepaySchema().load(data)
         return self._make_request(endpoint="/loans/repay", data=data)
 
-    def _make_request(self, endpoint, params=None, data=None):
+    def _make_request(self, endpoint: str, params: dict=None, data: dict=None):
         if not params:
             params = {}
         if not data:
@@ -81,9 +101,10 @@ class BNF_API:
             url=f"{self.host}{endpoint}",
             params=params,
             data=json.dumps(data),
+            headers={"Content-Type": "application/json"},
         )
 
-        return response.json()
+        return response.json(), response.status_code
 
     def sign_request(self, data: dict):
         qs_data = "&".join([f"{k}={v}" for k, v in data.items()])
